5ea1814a434bd64393d104d65d917b1aa8709fd3
amanmeherally <dacyerdash@gmail.com>
2025-05-02 14:27:18 -0400

    Removed dist

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Aman</h1>
+      <h1>Vite + React + Aman2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
