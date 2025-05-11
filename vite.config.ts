dbd49eded2b5e83196381a7a1edc7f1ce6abf292
amanmeherally <dacyerdash@gmail.com>
2025-05-11 17:20:33 -0400

    Added GitHub pages support

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  server: {
-    allowedHosts: [
-      "b226-2607-fea8-a5dc-f900-30bd-59f-5cd3-88ec.ngrok-free.app",
-    ],
-  },
+  base: "/portfolio",
 });
